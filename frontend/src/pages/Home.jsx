886ee1fb0c2f1f3c032cf1b86809cc8d8cdf8373
aryan <aryannagar193@gmail.com>
2024-12-26 19:47:52 +0530

    change Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     <>
       
     <h2 class="relative text-center text-xl my-4">
-  <span class="absolute left-0 w-[41.8%] top-1/2 transform -translate-y-1/2 border-t-2 border-black"></span>
+  <span class="absolute left-0 w-[38%] top-1/2 transform -translate-y-1/2 border-t-2 border-black"></span>
   EXPLORE OUR CATEGORIES
-  <span class="absolute right-0 w-[41.8%] top-1/2 transform -translate-y-1/2 border-t-2 border-black"></span>
+  <span class="absolute right-0 w-[38%] top-1/2 transform -translate-y-1/2 border-t-2 border-black"></span>
       </h2>
       <div>
         <CategoryList />
