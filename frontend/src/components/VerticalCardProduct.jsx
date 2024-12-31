4dd37876a0e10eab000a3d77e6f5e019d145bcf7
aryan <aryannagar193@gmail.com>
2025-01-01 01:18:11 +0530

    Add Loader state and made a skeleton for every product or component with some styling

--- frontend/src/components/VerticalCardProduct.jsx
+++ frontend/src/components/VerticalCardProduct.jsx
@@ -64,35 +64,67 @@ const VerticalCardProduct = ({ category, heading }) => {
         >
           <FaAngleRight />
         </button>
-        {data.map((product, index) => {
-          return (
-            <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
-              <div className="bg-slate-200 h-60 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
-                <img
-                  src={product.productImage[0]}
-                  className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
-                />
-              </div>
-              <div className="p-4 grid gap-3">
-                <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black">
-                  {product?.productName}{" "}
-                </h2>
-                <p className="text-slate-500">{product?.category}</p>
-                <div className="flex gap-3">
-                  <p className="text-red-600 font-medium">
-                    {displayCurrency(product?.sellingPrice)}
-                  </p>
-                  <p className="text-slate-500 line-through">
-                    {displayCurrency(product?.price)}
-                  </p>
+
+
+        {loading ? (
+          loadingList.map((product, index) => {
+            return (
+              <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
+                <div className="bg-slate-200 h-60 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse">
+                  
                 </div>
-                <button className="text-sm bg-red-600 hover:bg-red-700 text-white px-3 py-0.5 rounded-full">
-                  Add to Cart
-                </button>
+                <div className="p-4 grid gap-3 w-full">
+                  <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-3 animate-pulse rounded-full bg-slate-200">
+                    
+                  </h2>
+                  <p className="text-slate-500 p-1 py-2 rounded-full animate-pulse bg-slate-200"></p>
+                  <div className="flex gap-3 w-full">
+                    <p className="text-red-600 font-medium p-1 py-2 rounded-full animate-pulse bg-slate-200 w-full">
+                      
+                    </p>
+                    <p className="text-slate-500 line-through p-1 py-2 rounded-full animate-pulse bg-slate-200 w-full">
+                      
+                    </p>
+                  </div>
+                  <button className="text-sm text-white px-3 rounded-full p-1 py-2 animate-pulse bg-slate-200">
+                    
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          data.map((product, index) => {
+            return (
+              <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
+                <div className="bg-slate-200 h-60 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
+                  <img
+                    src={product.productImage[0]}
+                    className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
+                  />
+                </div>
+                <div className="p-4 grid gap-3">
+                  <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black">
+                    {product?.productName}{" "}
+                  </h2>
+                  <p className="text-slate-500">{product?.category}</p>
+                  <div className="flex gap-3">
+                    <p className="text-red-600 font-medium">
+                      {displayCurrency(product?.sellingPrice)}
+                    </p>
+                    <p className="text-slate-500 line-through">
+                      {displayCurrency(product?.price)}
+                    </p>
+                  </div>
+                  <button className="text-sm bg-red-600 hover:bg-red-700 text-white px-3 py-0.5 rounded-full">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        )
+          }
       </div>
     </div>
   );
