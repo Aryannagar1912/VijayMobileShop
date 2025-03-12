a5a05d005c48db788395f5d0d0eae146d03609ce
aryan <aryannagar193@gmail.com>
2025-03-12 20:04:53 +0530

    fix error

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -108,7 +108,7 @@ const Cart = () => {
     }
 
     const handlePayment = async () => {
-        const stripePromise = await loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+        const stripePromise = await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
         const response = await fetch(SummaryApi.payment.url, {
             method: SummaryApi.payment.method,
             credentials: 'include', 
