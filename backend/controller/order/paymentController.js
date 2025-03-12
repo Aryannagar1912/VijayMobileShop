5de2c6c7d2fbea79e5f764028bda6b17413743f5
aryan <aryannagar193@gmail.com>
2025-03-13 01:49:15 +0530

    fix error

--- backend/controller/order/paymentController.js
+++ backend/controller/order/paymentController.js
@@ -1,61 +1,75 @@
-const stripe = require('../../config/stripe');
-const userModel = require('../../models/userModel');
+const stripe = require('../../config/stripe')
+const userModel = require('../../models/userModel')
 
-const paymentController = async(req, res) => {
+const paymentController = async(request,response)=>{
     try {
-        const { cartItems } = req.body
+        const { cartItems } = request.body
 
-        const user = await userModel.findOne({ _id : req.userId })
+        const user = await userModel.findOne({ _id : request.userId })
 
         const params = {
-            submit_type: 'pay',
-            mode: "payment",
-            payment_method_types: ['card'],
-            billing_address_collection: 'auto',
-            shipping_option: [
+            submit_type : 'pay',
+            mode : "payment",
+            payment_method_types : ['card'],
+            billing_address_collection : 'auto',
+            shipping_options : [
                 {
                     shipping_rate : 'shr_1QzPXLFooVG8HRiU2lQnUo1M'
-                },
+                }
             ],
-            customer_email: user.email,
-            line_items: cartItems.map((item, index) => {
-                return {
-                    price_data: {
-                        currency: 'inr',
-                        product_data: {
-                            name: item.productId.productName,
-                            images: item.productId.productImage,
-                            metaData: {
-                                productId : item.productId._id
-                            }
-                        },
-                        unit_amount : item.productId.sellingPrice
+            customer_email : user.email,
+            metadata : {
+                userId : request.userId
+            },
+            line_items : cartItems.map((item,index)=>{
+                return{
+                    price_data : {
+                      currency : 'inr',
+                      product_data : {
+                        name : item.productId.productName,
+                        images : item.productId.productImage,
+                        metadata : {
+                            productId : item.productId._id
+                        }
+                      },
+                      unit_amount : item.productId.sellingPrice * 100
                     },
-                    adjustable_quantity: {
-                        enabled: true,
+                    adjustable_quantity : {
+                        enabled : true,
                         minimum : 1
                     },
-                    quantity : item.quantity 
+                    quantity : item. quantity
+
                 }
             }),
-
-            success_url: `${process.env.FRONTEND_URL}/success`,
-            cancel_url : `${process.env.FRONTEND_URL}/cancel`
+            success_url : `${process.env.FRONTEND_URL}/success`,
+            cancel_url : `${process.env.FRONTEND_URL}/cancel`,
         }
 
         const session = await stripe.checkout.sessions.create(params)
 
-        res.status(303).json(session)
+        response.status(303).json(session)
 
     } catch (error) {
-        Response.json({
-            message: error?.message || error,
-            error: true,
+        response.json({
+            message : error?.message || error,
+            error : true,
             success : false
         })
     }
 }
 
+module.exports = paymentController
 
 
-module.exports = paymentController;
+
+
+
+
+
+
+
+
+
+
+shr_1QzPXLFooVG8HRiU2lQnUo1M
