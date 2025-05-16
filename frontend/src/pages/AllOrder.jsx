dd1f359613ffb4190119aa990d17df7bd68a6378
aryan <aryannagar193@gmail.com>
2025-05-17 02:38:17 +0530

    change background color

--- frontend/src/pages/AllOrder.jsx
+++ frontend/src/pages/AllOrder.jsx
@@ -31,12 +31,12 @@ const AllOrder = () => {
              )
           }
     
-          <div className='p-4 w-full text-white'>
+          <div className='p-4 w-full'>
               {
                 data.map((item,index)=>{
                   return(
                     <div key={item.userId+index}>
-                       <p className='font-medium text-lg '>{moment(item.createdAt).format('LL')}</p> 
+                       <p className='font-medium text-lg text-white'>{moment(item.createdAt).format('LL')}</p> 
                        <div className='border rounded p-1'>
                             <div className='flex flex-col lg:flex-row justify-between'>
                                 <div className='grid gap-2'>
@@ -46,7 +46,7 @@ const AllOrder = () => {
                                         <div key={product.productId+index} className='flex  gap-3 bg-white'>
                                             <img 
                                               src={product.image[0]}
-                                              className='w-28 h-28 bg-slate-200 object-scale-down p-2'
+                                              className='w-28 h-28 bg-white object-scale-down p-2'
                                             />
                                             <div>
                                               <div className='font-medium text-lg text-ellipsis line-clamp-1'>{product.name}</div>
